6cab46a5643a8bc337f7d1a98c393b316ab2a332
Haniyeh Tajer <haniyehtajer@gmail.com>
2024-07-16 13:35:53 -0500

    changed inputs of ellipse

--- ellipse.py
+++ ellipse.py
@@ -1,6 +1,9 @@
 import numpy as np 
 
 #A function defining an ellipse
-def ellipse(phi,a,b):
+#inputs are angle (phi), semi-major axis (a),
+# and eccentricity (e)
+def ellipse(phi,a,e):
+    b = np.sqrt(1-e**2) * a 
     return np.array([a*np.cos(phi), b*np.sin(phi)])
 
