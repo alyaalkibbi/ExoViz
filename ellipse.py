515faaf373d152dc3fa812ae9d7c0a305402c2da
Alya Al-Kibbi <aalkibbi@caltech.edu>
2024-07-18 14:12:07 -0500

    Removed files and added docstrings

--- ellipse.py
+++
@@ -1,9 +0,0 @@
-import numpy as np 
-
-#A function defining an ellipse
-#inputs are angle (phi), semi-major axis (a),
-# and eccentricity (e)
-def ellipse(phi,a,e):
-    b = np.sqrt(1-e**2) * a 
-    return np.array([a*np.cos(phi), b*np.sin(phi)])
-
